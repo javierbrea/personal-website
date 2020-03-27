b0f51683414b7a51a5dd3aa99fcd59dd80fa92a9
javierbrea <javier.brea@gmail.com>
2020-03-27 20:09:44 +0100

    feat(arch): Add basic structure

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  globals: {
+    __PATH_PREFIX__: true,
+  },
+  "root": true,
+  "env": {
+    "browser": true,
+    "node": true,
+    "es6": true
+  },
+  "parser": "babel-eslint",
+  "plugins": ["prettier", "react", "react-hooks", "filenames"],
+  "extends": ["plugin:react/recommended", "prettier"],
+  "rules": {
+    "prettier/prettier": [
+      "error",
+      {
+        "printWidth": 99,
+        "parser": "flow"
+      }
+    ],
+    "no-undef": [2],
+    "no-unused-vars": [2, { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
+    "react/prop-types": 2,
+    "react/no-multi-comp": [2, { "ignoreStateless": false }],
+    "react/no-deprecated": [2],
+    "react/no-typos":[2],
+    "react/sort-prop-types": [2],
+    "react/jsx-sort-props": [2],
+    "react/jsx-no-bind": [2],
+    "react-hooks/rules-of-hooks": [2],
+    "react-hooks/exhaustive-deps": [2],
+    "filenames/match-regex": [0],
+    "filenames/match-exported": [2, "pascal"],
+    "filenames/no-index": [0]
+  },
+  "settings": {
+    "react": {
+      "pragma": "React",
+      "version": "^16.11.0"
+    }
+  }
+}
---
+++ gatsby-config.js
@@ -0,0 +1,41 @@
+/*
+Copyright 2020 Javier Brea
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+*/
+
+module.exports = {
+  siteMetadata: {
+    title: `Javier Brea`,
+    description: `Javier Brea personal website`,
+    author: `@javierbrea`
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`
+      }
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Javier Brea personal website`,
+        short_name: `Javier Brea`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#ffffff`,
+        display: `minimal-ui`,
+        icon: `src/images/javierbrea-icon.png` // This path is relative to the root of the site.
+      }
+    }
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ]
+};
