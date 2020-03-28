2daebb443c042123cecdfc83efac5506736485ca
javierbrea <javier.brea@gmail.com>
2020-03-28 19:55:41 +0100

    feat(seo): Add meta data

---
+++ src/modules/seo/Seo.js
@@ -0,0 +1,127 @@
+/*
+Copyright 2020 Javier Brea
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+*/
+
+import React from "react";
+import PropTypes from "prop-types";
+import Helmet from "react-helmet";
+import { useStaticQuery, graphql } from "gatsby";
+
+function SEO({ description, lang, meta, keywords, title, socialImage }) {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            siteUrl
+            title
+            description
+            author
+            keywords
+            socialImage
+          }
+        }
+      }
+    `
+  );
+
+  const metaDescription = description || site.siteMetadata.description;
+
+  const allKeywords = site.siteMetadata.keywords.concat(keywords);
+  const socialImageName = socialImage || site.siteMetadata.socialImage;
+
+  const fullTitle = `${title} | ${site.siteMetadata.title}`;
+  const socialImageUrl = `${site.siteMetadata.siteUrl}/assets/images/og/${socialImageName}.jpg`;
+
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang
+      }}
+      meta={[
+        {
+          name: "description",
+          content: metaDescription
+        },
+        {
+          property: "og:title",
+          content: fullTitle
+        },
+        {
+          property: "og:description",
+          content: metaDescription
+        },
+        {
+          property: "og:type",
+          content: "website"
+        },
+        {
+          property: "og:image",
+          content: socialImageUrl
+        },
+        {
+          name: "twitter:card",
+          content: "summary"
+        },
+        {
+          name: "twitter:site",
+          content: "@javierbrea"
+        },
+        {
+          name: "twitter:creator",
+          content: site.siteMetadata.author
+        },
+        {
+          name: "twitter:title",
+          content: fullTitle
+        },
+        {
+          name: "twitter:description",
+          content: metaDescription
+        },
+        {
+          name: "twitter:image",
+          content: socialImageUrl
+        },
+        {
+          name: "twitter:image:alt",
+          content: metaDescription
+        }
+      ]
+        .concat(
+          allKeywords.length > 0
+            ? [
+                {
+                  name: `keywords`,
+                  content: allKeywords.join(`, `)
+                }
+              ]
+            : []
+        )
+        .concat(meta)}
+      title={title}
+      titleTemplate={`%s | ${site.siteMetadata.title}`}
+    />
+  );
+}
+
+SEO.defaultProps = {
+  lang: `en`,
+  meta: [],
+  keywords: [],
+  description: ``
+};
+
+SEO.propTypes = {
+  description: PropTypes.string,
+  keywords: PropTypes.arrayOf(PropTypes.string),
+  lang: PropTypes.string,
+  meta: PropTypes.arrayOf(PropTypes.object),
+  socialImage: PropTypes.string,
+  title: PropTypes.string.isRequired
+};
+
+export default SEO;
