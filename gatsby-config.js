05d2c30a36efd0d7d37dade94ac4dd4d73c99d1c
javier.brea <javier.brea@gmail.com>
2020-11-14 14:11:46 +0100

    chore(deps): Update dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 */
 
+const sass = require("sass");
 const SITE_URL = "https://www.javierbrea.com";
 
 const {
@@ -41,6 +42,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-sass",
       options: {
+        implementation: sass,
         includePaths: [`${__dirname}/node_modules/bootstrap`],
       },
     },
