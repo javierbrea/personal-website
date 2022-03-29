e931560854ae4d01561865538d354c4885bef6b0
javierbrea <javier.brea@gmail.com>
2022-03-29 14:13:55 +0200

    chore: remove deprecated snapshots plugin

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,24 +1,24 @@
 import "@testing-library/cypress/add-commands";
-import { addMatchImageSnapshotCommand } from "cypress-image-snapshot/command";
+// import { addMatchImageSnapshotCommand } from "cypress-image-snapshot/command";
 
 import Browser from "./page-objects/common/Browser";
 
 const browser = new Browser();
 
-addMatchImageSnapshotCommand({
+/* addMatchImageSnapshotCommand({
   failureThreshold: 0.03, // threshold for entire image
   failureThresholdType: "percent", // percent of image or number of pixels
   customDiffConfig: { threshold: 0.1 }, // threshold for each pixel,
   customDiffDir: "cypress/screenshots",
   capture: "viewport", // capture viewport in screenshot
-});
+}); */
 
 Cypress.Commands.add("setWindowWidth", (width) => {
   browser.setViewPort(width);
 });
 
 Cypress.Commands.add("shouldMatchSnapshot", () => {
-  if (Cypress.env("VISUAL_TESTS")) {
+  /* if (Cypress.env("VISUAL_TESTS")) {
     cy.matchImageSnapshot();
-  }
+  } */
 });
