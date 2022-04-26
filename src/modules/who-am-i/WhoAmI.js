7fb9b4e339ff721e0283c945c850b7b4c9290150
javierbrea <javier.brea@gmail.com>
2022-04-26 09:48:58 +0200

    feat: improve who am i

--- src/modules/who-am-i/WhoAmI.js
+++ src/modules/who-am-i/WhoAmI.js
@@ -15,16 +15,19 @@ const WhoAmI = () => {
       <Title centered>Who am I</Title>
       <div className={classes.text}>
         <p>
-          I&lsquo;m currently working as head of front-end software development at Telefonica Tech.
+          I&rsquo;m currently working as head of front-end software development at Telefonica Tech.
         </p>
         <p>
-          I&lsquo;m writing code professionally since 2003, working as a front-end specialist since
-          2010, and as a front-end architect since 2015.
+          I&rsquo;m writting code professionally since 2003, working as a front-end specialist
+          since 2010, and as a front-end architect since 2015.
         </p>
         <p>
           My main skills are: building web apps in JavaScript & Node.js, CI/CD, development tools,
-          E2E testing and data modeling. I am known for being a high quality code advocate, modular
-          solutions oriented, detail oriented and opensource author.
+          E2E testing and data modeling.
+        </p>
+        <p>
+          I am known for being methodical and detail oriented, a high quality code advocate,
+          oriented to modular solutions, and opensource author.
         </p>
       </div>
     </div>
